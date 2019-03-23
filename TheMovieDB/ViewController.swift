cdc4715842a58b0495159ca93b22420808d83415
Günter <gunter.mhertz@gmail.com>
2019-03-23 20:52:07 -0300

    Create basic project

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Günter Hertz on 23/03/19.
+//  Copyright © 2019 Günter Hertz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
