98fa8fa334c7a2ec7d66ce96fa328d804eaa2b91
Günter <gunter.mhertz@gmail.com>
2019-03-24 20:53:25 -0300

    Create a movie cell

---
+++ TheMovieDB/View/Cell/MovieCell.swift
@@ -0,0 +1,52 @@
+//
+//  MovieCell.swift
+//  TheMovieDB
+//
+//  Created by Günter Hertz on 24/03/19.
+//  Copyright © 2019 Günter Hertz. All rights reserved.
+//
+
+import SnapKit
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    let image: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        return view
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        viewSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup(posterPath: String) {
+        //fill the image
+        let imageURL = "http://image.tmdb.org/t/p/w185/" + posterPath
+        print(imageURL)
+        image.getImageFromURL(url: imageURL)
+    }
+}
+
+extension MovieCell: CodeView {
+    func viewHierarchy() {
+        addSubview(image)
+    }
+    
+    func viewConstraints() {
+        image.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.left.equalToSuperview()
+        }
+    }
+    
+    func viewAdditionalConfiguration() {
+        
+    }
+}
